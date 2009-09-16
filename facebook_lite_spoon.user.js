81f07e3d48b970e7072c366125025c9a9cf294ab
Marcus Carlsson <carlsson.marcus@gmail.com>
2009-09-16 14:25:52 +0200

    Added a profilethumbnail hover function that shows the original sized image at the upper right corner.

--- facebook_lite_spoon.user.js
+++ facebook_lite_spoon.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name          Facebook Lite Spoon
 // @namespace     http://userscripts.org/scripts/show/57854
-// @description	  Removes the right-hand bar from Facebook Lite, expanding the content to 60% width. Removes Facebook | from the title and makes the header fixed at the top.
+// @description	  Removes the right-hand bar from Facebook Lite, expanding the content to 60% width. Removes Facebook | from the title and makes the header fixed at the top. Thumbnail previewer.
 // @include       http://lite.facebook.com/*
 // @author		  Marcus Carlsson
-// @version		  0.1.6
+// @version		  0.2
 // ==/UserScript==
 
 (function () {
@@ -22,6 +22,33 @@ addStyle(' #header { position:fixed !important; width:100% !important; z-index:1
 '#content { padding-top:' + contentPosition[1] + 'px; }');
 
 //
+// Creates a box where the original image is shown when hovering profile thumbnails
+//
+function showHover() {
+	if (!$('FLSpopup')) {
+		var FLSpopup = document.createElement('div');
+		FLSpopup.id = 'FLSpopup';
+		addStyle("#FLSpopup { background: #ffffff; position: fixed !important; top: 20px; right: 20px; z-index: 20; border: 1px solid #333333; padding: 5px; display: none; }");
+		document.body.appendChild(FLSpopup);
+	}
+	var image = document.createElement('img');
+	image.src = this.src.replace(/\/[aqst]([\d_]+)\.jpg/, "/n$1.jpg").replace(/\/([\d_]+)[aqst]\.jpg/, "/$1n.jpg");
+	$('FLSpopup').innerHTML = '';
+	$('FLSpopup').appendChild(image);
+	$('FLSpopup').style.display = 'block';
+}
+
+function hideHover() {
+	$('FLSpopup').style.display = '';
+}
+
+var profilephoto = $$('profilePhoto');
+for (i in profilephoto) {
+	profilephoto[i].childNodes[0].addEventListener('mouseover', showHover, false);
+	profilephoto[i].childNodes[0].addEventListener('mouseout', hideHover, false);
+}
+
+//
 // Remove Facebook from the title
 //
 var titleValue = document.title;
@@ -31,6 +58,25 @@ document.title = titleValue.replace('Facebook | ', '');
 // Get element by id
 //
 function $(id,root){return root ? root.getElementById(id) : document.getElementById(id);}
+
+//
+// Get element(s) by class name
+//
+function $$(className,root){
+	if (document.getElementsByClassName) {
+		return root ? root.getElementsByClassName(className) : document.getElementsByClassName(className);
+	} else {
+		var elms = $x('//*[contains(@class,"'+className+'")]',root);
+		var buffer = new Array();
+		for (var i=0; i<elms.snapshotLength; i++) { buffer.push(elms.snapshotItem(i)); }
+		return buffer;
+	}
+}
+
+//
+// XPath
+//
+function $x(xpath,root){return document.evaluate(xpath,(root?root:document),null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,null);}
 
 //
 // Add style

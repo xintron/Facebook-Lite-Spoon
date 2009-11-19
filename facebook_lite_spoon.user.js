02df88f79c60380ee554b9474dda01237107998b
Marcus Carlsson <carlsson.marcus@gmail.com>
2009-11-19 11:36:38 +0100

    Updates to work with the new id/classes on facebook. Some work done on photo-albums to enable hovering images to view full resolution images.

--- facebook_lite_spoon.user.js
+++ facebook_lite_spoon.user.js
@@ -1,3 +1,28 @@
+// Copyright (c) 2009, Marcus Carlsson <carlsson.marcus@gmail.com>
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the <organization> nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY Marcus Carlsson ''AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL <copyright holder> BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
 // ==UserScript==
 // @name          Facebook Lite Spoon
 // @namespace     http://userscripts.org/scripts/show/57854
@@ -5,15 +30,22 @@
 // @include       http://lite.facebook.com/*
 // @include       https://lite.facebook.com/*
 // @author		  Marcus Carlsson
-// @version		  0.3.4
+// @version		  0.3.5
 // ==/UserScript==
 
 (function () {
 
 //
+// Check if we're on the main page, then hide the ads
+//
+if ($$('LMuffinView', $$('LSplitPage_RightInner')[0])[0]) {
+	addStyle(".LSplitPage_Right {display: none;}");
+}
+
+//
 // Fixes the width of the page and removes the right-hand bar, also set comments to 100% width
 //
-addStyle("#navigation, #content, #footer {width: 60%; min-width: 900px;} .feedbackView { width: 100%; }");
+addStyle("#navigation, #content, #footer {width: 80%; min-width: 900px;} .FN_feedbackview, .UFIView { width: 100%; }");
 
 //
 // Top Bar Positioning
@@ -22,28 +54,18 @@ var contentPosition = getPosition($('content'));
 addStyle(' #header { position:fixed !important; width:100% !important; z-index:12; margin-top:0; } '+
 '#content { padding-top:' + contentPosition[1] + 'px; }');
 
-
-//
-// Check if we're on the main page, then hide the ads
-//
-if ($$('LMuffinView', $$('LSplitView_RightInner')[0])[0]) {
-	addStyle(".LSplitView_Right {display: none;}");
-}
-
 //
 // Viewing photos, set the comments to 100% width and check for browser-resize
 //
 function photoComments() {
-	var size = ($('contentWrapper').offsetWidth - $$('LSplitView_Content')[0].offsetWidth - 80);
-	addStyle(".LPhideoView .LSplitView_Right {width: "+size+"px;} .LPhideoView .UFIView, .UFIView {width: 100%;}") ;
+	var size = ($('contentWrapper').offsetWidth - 625 -80);
+    addStyle('.LPhotoListView .LSplitPage_Right {width: '+size+'px;} .LPhotoListView .LSplitPage_Content {width: 605px;} .LPhotoListView .UFIView, .LPhotoListView .LSplitPage_Right .LSplitPage_RightInner {width: 100%}');
 }
-photoComments();
-window.addEventListener('resize', photoComments, false);
 
 //
 // Creates a box where the original image is shown when hovering profile thumbnails
 //
-function showHover() {
+function showHover(e) {
 	if (!$('FLSpopup')) {
 		var FLSpopup = document.createElement('div');
 		FLSpopup.id = 'FLSpopup';
@@ -51,7 +73,11 @@ function showHover() {
 		document.body.appendChild(FLSpopup);
 	}
 	var image = document.createElement('img');
-	image.src = this.src.replace(/\/[aqst]([\d_]+)\.jpg/, "/n$1.jpg").replace(/\/([\d_]+)[aqst]\.jpg/, "/$1n.jpg");
+    console.log(this);
+    if (this.tagName == 'I')
+        image.src = this.style.backgroundImage.replace(/url\(([^)]+)\)/i, '$1').replace(/\/[aqst]([\d_]+)\.jpg/, "/n$1.jpg").replace(/\/([\d_]+)[aqst]\.jpg/, "/$1n.jpg");
+    else
+        image.src = this.src.replace(/\/[aqst]([\d_]+)\.jpg/, "/n$1.jpg").replace(/\/([\d_]+)[aqst]\.jpg/, "/$1n.jpg");
 	$('FLSpopup').innerHTML = '';
 	$('FLSpopup').appendChild(image);
 	$('FLSpopup').style.display = 'block';
@@ -82,9 +108,11 @@ function mouseEvents() {
 		}			
 	}
 
-	// Add for photogallery
+	// Add for photogallery as well as setting width for comments to 100% and 50% of #content
 	if (document.getElementsByTagName('body')[0].getAttribute('class').match('LPhotoListView') || document.getElementsByTagName('body')[0].getAttribute('class').match('LProfilePhotoPane')) {
-		var galleryimg = document.getElementsByTagName('td');
+        photoComments();
+        window.addEventListener('resize', photoComments, false);
+        var galleryimg = $$('LGridCropView')[0].getElementsByTagName('td');
 		for (i in galleryimg) {
 			var nodes = galleryimg[i].childNodes;
 			for (n in nodes) {
